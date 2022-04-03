12253329098bff109d3610b67aa14573fe255029
Alexis Murzeau <amubtdx@gmail.com>
2022-04-03 19:09:26 +0200

    rest url: use different config according to prod/dev

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ConfigService {
-    public static readonly baseUrl: string = "/";
+    public static readonly baseUrl: string = environment.restBaseUrl;
 }
